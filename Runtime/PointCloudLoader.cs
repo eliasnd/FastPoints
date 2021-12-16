80d22c0f57ce893065dc7f36c3312fcf283422c4
Elias Neuman-Donihue <elias@ndcoding.com>
2021-12-16 17:03:09 -0500

    Chunking merging and writing compiles

--- Runtime/PointCloudLoader.cs
+++ Runtime/PointCloudLoader.cs
@@ -2,19 +2,31 @@ using UnityEngine;
 using UnityEditor;
 
 using System;
+using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace FastPoints {
     [ExecuteInEditMode]
     class PointCloudLoader : MonoBehaviour {
+
+        #region public
         public PointCloudData data;
         public ComputeShader countShader;
         public ComputeShader sortShader;
 
+        #endregion
+
+        #region checkpoints
+
         bool loading = false;
-        bool reading; // Whether data has started reading from file
+        bool reading = false; // Whether data has started reading from file
+        bool merging = false;
+        bool writing = false;
+
+        #endregion
 
         // Parameters for reading
         int batchSize = 20000;
@@ -25,9 +37,10 @@ namespace FastPoints {
         // RenderTexture counts; // Counts for chunking
         ComputeBuffer countBuffer; // Counts for chunking, flattened
         uint[] counts;
+        uint[] mergeCounts;
+        List<int> mergeIndices; // List of which indices are being used to represent merged chunks
         bool boundsPopulated = false;
         uint pointsCounted = 0;
-        bool writing; // Whether data has started writing to file
         float progress = 0.0f;
         Queue<Point[]> countedBatches;
 
@@ -35,6 +48,7 @@ namespace FastPoints {
             if (data == null) {
                 loading = false;
                 reading = false;
+                merging = false;
                 writing = false;
                 pointsCounted = 0;
                 boundsPopulated = false;
@@ -61,6 +75,8 @@ namespace FastPoints {
                 // Use compute buffer instead
                 countBuffer = new ComputeBuffer(chunkCount * chunkCount * chunkCount, sizeof(UInt32));
                 counts = new uint[chunkCount * chunkCount * chunkCount];
+                mergeCounts = new uint[chunkCount * chunkCount * chunkCount];
+                mergeIndices = new List<int>();
 
                 int countHandle = countShader.FindKernel("CountPoints"); // Initialize count shader
                 countShader.SetBuffer(countHandle, "_Counts", countBuffer);
@@ -116,17 +132,18 @@ namespace FastPoints {
                 }
                 if (pointsCounted == data.PointCount) {
                     reading = false;
-                    writing = true;
+                    merging = true;
                     countBuffer.GetData(counts);
                     Debug.Log("Done counting");
                 }
             }
 
+            if (merging) {
+                MergeSparseChunks(counts);
+                merging = false;
+            }
+
             if (writing) {
-                int sum = 0;
-                for (int i = 0; i < chunkCount * chunkCount * chunkCount; i++)
-                    sum += (int)counts[i];
-                Debug.Log("Total points counted: " + sum);
                 if (countedBatches.Count == 0) {
                     writing = false;
                     return;
@@ -181,6 +198,94 @@ namespace FastPoints {
             data.LoadPointBatches(batchSize, pointBatches);
             reading = true;
         }
-        
+
+        // Populates merge count array -- for each chunk merged, gets assigned index of lowest (x,y,z) coordinate of merged chunk
+        async void MergeSparseChunks(uint[] counts) {
+            int sparseCutoff = 100;
+
+            Func<(int, int, int), uint> getCount = tup => counts[tup.Item1 * chunkCount * chunkCount + tup.Item2 * chunkCount + tup.Item3];
+            Func<int, int, int, int, int, int, uint> countChunk = delegate(int x1, int x2, int y1, int y2, int z1, int z2) {
+                uint sum = 0;
+                for (int i = x1; i < x2; i++)
+                    for (int j = y1; j < y2; j++)
+                        for (int k = z1; k < z2; k++)
+                            sum += getCount((i,j,k));
+                return sum;
+            };
+
+            Action<int, int, int, int, int, int, uint> markChunk = delegate(int x1, int x2, int y1, int y2, int z1, int z2, uint mergeVal) {
+                for (int i = x1; i < x2; i++)
+                    for (int j = y1; j < y2; j++)
+                        for (int k = z1; k < z2; k++) {
+                            int chunkIdx = i * chunkCount * chunkCount + j * chunkCount + k;
+                            mergeCounts[chunkIdx] = mergeVal;
+                            if (mergeIndices.Contains(chunkIdx))
+                                mergeIndices.Remove(chunkIdx);
+                        }
+                mergeIndices.Add((int)mergeVal);
+            };
+            
+
+            await Task.Run(() => {
+                for (uint i = 0; i < counts.Length; i++) {
+                    mergeCounts[i] = i;
+                    mergeIndices.Add((int)i);
+                }
+
+                for (int size = 2; size < chunkCount; size *= 2) {
+                    for (int i = 1; i <= chunkCount / size; i++) {
+                        for (int j = 1; j <= chunkCount / size; j++) {
+                            for (int k = 1; k <= chunkCount / size; k++) {
+                                if (countChunk((i-1)*size,i*size,(j-1)*size,j*size,(k-1)*size,k*size) <= sparseCutoff) {
+                                    markChunk((i-1)*size,i*size,(j-1)*size,j*size,(k-1)*size,k*size, (uint)((i-1)*size));
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            writing = true;
+        }
+
+        async void CreateChunkFiles(string root = "") {
+            await Task.Run(() => {
+               for (int i = 0; i < mergeIndices.Count; i++) {
+                    String path = root != "" ? $"{root}/chunk{i}.ply" : $"chunk{i}.ply";
+                    String header = $@"ply
+                    format binary_little_endian 1.0
+                    element vertex {mergeCounts[mergeIndices[i]]}
+                    property float x
+                    property float y
+                    property float z
+                    property uchar red
+                    property uchar green
+                    property uchar blue
+                    end_header
+                    ";
+                    System.IO.File.WriteAllText(path, header);
+               } 
+            });
+        }
+
+        // Create binary ply files for tree nodes
+        async void WriteChunk(int chunkIdx, Point[] points, string root = "") {
+            await Task.Run(() => {
+
+                String path = root == "" ? $"chunk{chunkIdx}.ply" : $"{root}/chunk{chunkIdx}.ply";
+
+                BinaryWriter bw = new BinaryWriter(File.Open(path, FileMode.Append));
+                for (int i = 0; i < points.Length; i++) {
+                    Point pt = points[i];
+                    bw.Write(pt.pos.x);
+                    bw.Write(pt.pos.y);
+                    bw.Write(pt.pos.z);
+                    bw.Write(Convert.ToByte((int)(pt.col.r * 255.0f)));
+                    bw.Write(Convert.ToByte((int)(pt.col.g * 255.0f)));
+                    bw.Write(Convert.ToByte((int)(pt.col.b * 255.0f)));
+                }
+                bw.Dispose();
+            });
+        }
     }
 }
